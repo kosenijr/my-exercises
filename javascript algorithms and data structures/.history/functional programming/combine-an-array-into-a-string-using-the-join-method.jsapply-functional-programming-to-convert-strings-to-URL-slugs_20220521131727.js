ec99cac2a05bd4ba2c1779f9de5707a0403a875a
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2022-05-21 15:00:19 -0400

    filled in empty templates with lessons and activities for intermediate algorithmic scripting.

---
+++ javascript algorithms and data structures/.history/functional programming/combine-an-array-into-a-string-using-the-join-method.jsapply-functional-programming-to-convert-strings-to-URL-slugs_20220521131727.js
@@ -0,0 +1,11 @@
+/*
+function splitify(str) {
+  // Only change code below this line
+
+
+  // Only change code above this line
+}
+
+splitify("Hello World,I-am code");
+________________________________________________________________
+*/
