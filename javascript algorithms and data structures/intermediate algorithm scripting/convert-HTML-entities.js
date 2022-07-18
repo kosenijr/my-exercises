3cea2578959808fc93a5fe0518debad6dfe7c200
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2022-07-18 14:08:04 -0400

    convert-HTML-entities.js lesson and activity completed

--- javascript algorithms and data structures/intermediate algorithm scripting/convert-HTML-entities.js
+++ javascript algorithms and data structures/intermediate algorithm scripting/convert-HTML-entities.js
@@ -18,8 +18,39 @@ convertHTML("<>") should return the string &lt;&gt;.
 convertHTML("abc") should return the string abc.
 ________________________________________________________________
 */
+/*
+// abbrev logs to console. then check.
+const log = console.log;
+// log('let\'s code!');
+// create function to convert HTML after taking in a string param.
 function convertHTML(str) {
+    // log(str);
+    // create subarrays of special characters and their corresponding HTML entities
+    // let arr = [['&', '&amp;'], ['<', '&lt;'], ['>', '&gt;'], ['"', '&quot;'], ['\'', '&apos;']];
+    let arr = [['&', '&amp;'], ['<', '&lt;'], ['>', '&gt;'], ['"', '&quot;'], ['\'', '&apos;']];
+    // check each element
+    // arr.forEach(elem => log(elem[0], `\n`, elem[1]));
+    // set up regex through map method
+    arr.map(elem => str.includes(elem[0]) ? str = str.replaceAll(`${elem[0]}`, elem[1]) : null)
+    // arr.map(elem => str.includes(elem[0]) ? str = str.replace(/&/g, 'n') : null)
+    // check str
+    log(str);
+    return str;
+}
+*/
+// _____________________________________________________________
+// Concise
+const log = console.log;
+function convertHTML(str) {
+    let arr = [['&', '&amp;'], ['<', '&lt;'], ['>', '&gt;'], ['"', '&quot;'], ['\'', '&apos;']];
+    arr.map(elem => str.includes(elem[0]) ? str = str.replaceAll(`${elem[0]}`, elem[1]) : null)
     return str;
 }
 
+// _____________________________________________________________
 convertHTML("Dolce & Gabbana");
+convertHTML("Hamburgers < Pizza < Tacos");
+convertHTML("Sixty > twelve");
+convertHTML('Stuff in "quotation marks"');
+convertHTML("<>");
+convertHTML("abc");
--- javascript algorithms and data structures/intermediate algorithm scripting/sorted-union.js
+++ javascript algorithms and data structures/intermediate algorithm scripting/sorted-union.js
@@ -1,4 +1,8 @@
 /*
+DON'T FORGET THE USE OF THE ARGUMENTS OBJECT FOR FUNCTION PARAMS
+ */
+
+/*
 Sorted Union
 
 Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
@@ -20,8 +24,43 @@ uniteUnique([1, 3, 2], [5, 4], [5, 6]) should return [1, 3, 2, 5, 4, 6].
 uniteUnique([1, 3, 2, 3], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
 ________________________________________________________________
 */
+
+/*
+// abbrev logs to console. then check.
+const log = console.log;
+log('let\'s go!');
+// create a function: takes two or more arrays and returns one unique array
+// be sure to include spread operator and new variable to represent multiple-array parameter
 function uniteUnique(arr) {
+    // combine all arrays within param, restore in arguments
+    let args = [...arguments].flat();
+    // log(args, Array.isArray(args)); // is in fact an array
+    // set up for loop for arr navigation
+    for (let k = 0; k < arr.length; k++) {
+        // compare iterator/indices to the result of the indexOf method
+        // log(arr[k], k === arr.indexOf(arr[k])) // got a falsy for the duplicate
+        k === arr.indexOf(arr[k]) ? null : arr.splice(arr[k]); // possible counter-duplication solution
+    }
+
+    // ternary condition(s)
+    args.map(elem => arr.indexOf(elem) === -1 ? arr.push(elem) : null);
+    // check arr
+    log(arr);
+
+
+    return arr;
+*/
+// ______________________________________________________________
+// Concise
+
+// abbrev logs to console. then check.
+const log = console.log;
+function uniteUnique(arr) {
+    let args = [...arguments].flat();
+    for (let k = 0; k < arr.length; k++) { k === arr.indexOf(arr[k]) ? null : arr.splice(arr[k]) }
+    args.map(elem => arr.indexOf(elem) === -1 ? arr.push(elem) : null);
     return arr;
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+uniteUnique([1, 3, 2, 3], [5, 2, 1, 4], [2, 1]);
