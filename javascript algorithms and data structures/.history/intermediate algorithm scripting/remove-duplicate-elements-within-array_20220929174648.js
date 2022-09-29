d123d3cd183d2173ac38c373615a8ab101bfc423
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2022-09-29 17:46:58 -0400

    completed personal task for quickly sifting through duplicate elements within an array

--- javascript algorithms and data structures/.history/intermediate algorithm scripting/remove-duplicate-elements-within-array_20220929174648.js
+++ javascript algorithms and data structures/.history/intermediate algorithm scripting/remove-duplicate-elements-within-array_20220929174648.js
@@ -1,6 +1,6 @@
 // create an array with duplicative values, declare and abbrev log, then chack.
 const [oldArr, newArr, log] = [[2, 3, 4, 5, 6, 3, 2, 8, 20], [], console.log];
-// log(arr); // laogs [2, 3, 4,  5, 6,3, 2, 8, 20]
+// log(arr); // logs [2, 3, 4,  5, 6,3, 2, 8, 20]
 
 // ternary condition to sift through oldArr elements
 oldArr.filter(elem => !newArr.includes(elem) ? newArr.push(elem) : null);
